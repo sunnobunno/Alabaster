2d22d573d83c6b787e0b742604c67bfc1acca7e2
Sunny <outofinkinc@gmail.com>
2024-02-26 15:09:43 -0500

    starting dice system

---
+++ Assets/DialogueSystem/Scripts/Controllers/SkillCheck/DiceController.cs
@@ -0,0 +1,29 @@
+using Alabaster.DialogueSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceController : DialogueElement
+{
+    
+
+    protected override void SetFields()
+    {
+        
+    }
+
+    protected override void SetReferences()
+    {
+        
+    }
+
+    public override void GreyOut(bool isGrey)
+    {
+
+    }
+
+    public override void ResizeElement()
+    {
+
+    }
+}
